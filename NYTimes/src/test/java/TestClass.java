5d1a4826983b904c09890fb311af0ae5794630ce
Momar2 <marwa_abu@hotmail.com>
2019-01-09 13:59:41 -0500

    add Marwa

---
+++ NYTimes/src/test/java/TestClass.java
@@ -0,0 +1,13 @@
+/*import base.CommonAPI2;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class TestClass extends CommonAPI2 {
+
+    @Test
+    public void Test1() {
+
+        driver.findElement(By.cssSelector("#twotabsearchtextbox")).sendKeys("Den of Thieves");
+
+    }
+}*/
